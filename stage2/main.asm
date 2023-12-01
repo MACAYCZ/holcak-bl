cc5bfdde938c667ba325970756b413d95d905a88
Petr Holčák <petr7698@gmail.com>
2023-12-01 22:05:45 +0100

    GAS is working!

--- stage2/main.asm
+++ stage2/main.asm
@@ -1,13 +1,16 @@
 .intel_syntax noprefix
 .code16
 
-.section .text
+.section .entry
 .global _start
 
 _start:
 	mov al, 'A'
 	mov ah, 0x0E
 	int 0x10
+	jmp main
 
+.section .text
+main:
 	cli
 	hlt
