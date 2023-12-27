c4d77dc2909c9fbbfce575833e3dfc24cbbf1228
Petr Holčák <petr7698@gmail.com>
2023-12-27 10:44:23 +0100

    Added CHANGELOG.md

--- stage2/memory/info.c
+++ stage2/memory/info.c
@@ -1,5 +1,5 @@
+#include <stage2/interface/print.h>
 #include <stage2/driver/x86_16.h>
-//#include <stage2/printf.h>
 #include "info.h"
 
 mem_info_t mem_init(void)
@@ -7,13 +7,13 @@ mem_info_t mem_init(void)
 	mem_info_t self;
 	x86_16_regs_t input, output;
 
-//	printf("Initializing Memory... ");
+	printf("Initializing Memory... ");
 
 	input.eax = 0xE801;
 	x86_16_int(0x15, &input, &output);
 
 	if (output.eflags & x86_EFLAGS_CARRY || (output.eax & 0xFF00) == 0x8000 || (output.eax & 0xFF00) == 0x8600) {
-//		printf("Failed!\n");
+		printf("Failed!\n");
 		while (1);
 	}
 
@@ -24,7 +24,7 @@ mem_info_t mem_init(void)
 
 	self.lower = output.eax;
 	self.upper = output.ebx;
-//	printf("Ok!\n\t| Lower: %hx\n\t| Upper: %hx\n", self.lower, self.upper);
+	printf("Ok!\n    Lower: %hx\n    Upper: %hx\n", self.lower, self.upper);
 
 	return self;
 }
